edfcf5310ae2f725d08d88ce1ceab618ce47932f
jjhageman <jjhageman@gmail.com>
2011-11-08 22:46:17 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapbadger::Application.config.secret_token = '10f1afc778665035f5d6f6d5eff76e2b3f4aadf6bf12d4e508b9cf167388096c1b1d767a2c2feba4b4174a4aa6c30ff9721e1d7fdfdcbd0a2d23b2b566257c52'
