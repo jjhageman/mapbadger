f0cec89f8a8f5e442470ec80bfdc7f1de4e903c1
Jeremy Hageman <jhageman@affinitylabs.com>
2011-12-29 13:35:57 -0800

    territory name uniqueness validation

--- app/models/territory.rb
+++ app/models/territory.rb
@@ -3,6 +3,8 @@ class Territory < ActiveRecord::Base
   has_many :regions, :through => :territory_regions
   accepts_nested_attributes_for :territory_regions, :allow_destroy => true
 
+  validates :name, :uniqueness => true
+
   def as_json(options = nil)
     super((options || {}).merge(include: { regions: { only: [:id, :name] } }))
   end
