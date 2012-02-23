b8e2faf77a814cd46b90dad3553c7380e16519ee
jjhageman <jjhageman@gmail.com>
2012-02-23 00:11:11 -0800

    migration hot fix

--- db/migrate/20120220003705_add_region_id_to_zcta.rb
+++ db/migrate/20120220003705_add_region_id_to_zcta.rb
@@ -1,7 +1,7 @@
 class AddRegionIdToZcta < ActiveRecord::Migration
   def change
     add_column :zcta, :region_id, :integer
+    add_index :zcta, :region_id
   end
 
-  add_index :zcta, :region_id
 end
