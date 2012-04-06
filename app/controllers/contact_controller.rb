744f9455ee215781f8fccaca38dcb86fecfb4ff9
jjhageman <jjhageman@gmail.com>
2012-04-06 00:45:32 -0700

    fix contact emailer

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,7 +1,8 @@
 class ContactController < ApplicationController
+  before_filter :authenticate_company!
   respond_to :html, :json
 
   def create
-    ContactMailer.wish_email(request.referrer, params[:message]).deliver unless params[:message].blank?
+    ContactMailer.wish_email(request.referrer, current_company.email, params[:message]).deliver unless params[:message].blank?
   end
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,10 @@
 class ContactMailer < ActionMailer::Base
   default from: 'feedback@mapbadger.com'
 
-  def wish_email(url, message)
+  def wish_email(url, sender, message)
     @url = url
     @message = message
-    @sender = current_company.try(:email)
+    @sender = sender
     mail to: 'hello@mapbadger.com', subject: 'I Wish Submission'
   end
 end
